290f08cdc571d93fda6ec3a3f5a871bb03221939
hiroshi_sato <h.sato@diamondhead.tech>
2021-03-15 10:26:29 +0900

    refactor: 💡 別ファイルだとうまく動かないので一旦まとめる
    ✅ Closes: #19

--- node/app.js
+++ node/app.js
@@ -22,9 +22,54 @@ app.use(express.urlencoded({ extended: true }));
 const hash = bcrypt.hashSync(process.env.ACCESS_TOKEN_SECRET, 10);
 
 // apiserverの読み込み
-const apiServer = require('./apiServer');
-apiServer(hash);
+app.post('/login', (req, res) => {
+    const payload = {
+        user: req.body.user
+    };
+
+    const option = {
+        expiresIn: sessionRetentionTime
+    };
+
+    const token = jwt.sign(payload, hash, option);
+
+    res.json({
+        message: 'create token',
+        token: token
+    });
+});
 
 // 認証用ミドルウェアの読み込み
-const authMiddle = require('./authMiddle');
-authMiddle(hash);
+const auth = (res, req, next) => {
+    // リクエストヘッダーからトークンの取得
+    let token = '';
+
+    if (req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer') {
+        token = req.headers.authorization.split(' ')[1]
+    } else {
+        return next('token none')
+    }
+
+    // トークンの検証
+    jwt.verify(token, hash, function(err, decoded) {
+        if (err) {
+            // 認証エラーの時
+            next(err.message)
+        } else {
+            // 認証OK
+            req.decoded = decoded
+            next()
+        }
+    });
+}
+
+// 認証必須API
+app.get('/user', auth, (req, res, next) => {
+    res.status(200).send('your name is ${req.decoded.user}')
+})
+
+// エラーハンドリング
+app.use((err, req, res, next) => {
+    res.status(500).send(err)
+})
